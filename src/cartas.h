be514cf2abb8d3fa3e06af5da13b6c49a725bdd9
Roger Alencar <51916539+rogeralencar@users.noreply.github.com>
2022-07-10 22:08:51 -0300

    Delete src directory

--- src/cartas.h
+++
@@ -1,6 +0,0 @@
-#ifndef CARTAS_H
-#define CARTAS_H
-
-void mostrarCartas(Lista *l);
-
-#endif
--- src/comandos.h
+++
@@ -1,6 +0,0 @@
-#ifndef COMANDOS_H
-#define COMANDOS_H
-
-void opcoesDoJogo(Lista **arr, Pilha **p, Lista *f);
-
-#endif
